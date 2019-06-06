a4ad22bb8fd91d409a79e8cd32ebfd95d3aca9cb
Tóth Attila <toth6.attila@ext.t-systems.hu>
2019-06-06 15:22:27 +0200

    the Root wont navigate & add card the subject in home

--- tanya/tanya-frontend/src/app/components/public-home.component.ts
+++ tanya/tanya-frontend/src/app/components/public-home.component.ts
@@ -1,12 +1,23 @@
 import { OnInit, Component } from '@angular/core';
+import { AuthService } from '../service/auth.service';
+import { AuthguardGuard } from '../authguard/authguard.guard';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-public-home-component',
     templateUrl: './public-home.component.html',
+    providers: [AuthguardGuard]
 })
 export class PublicHomeComponent implements OnInit {
 
+    constructor(private authGuard: AuthguardGuard, private route: Router) {}
+
     ngOnInit() {
-        console.log('PublicHomeComponent');
+        this.authGuard.refresh();
+        if (this.authGuard.isLogged) {
+            this.route.navigate(['/home']);
+        } else {
+            this.route.navigate(['/login']);
+        }
     }
 }
